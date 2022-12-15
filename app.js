0eed5bd758463996eb3c444b298cb00b1bf10378
tita <agostina.tizzano@est.fi.uncoma.edu.ar>
2022-12-14 21:36:15 -0300

    first commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath=path.resolve(__dirname, './public');
 
 app.use(express.static(publicPath));
 
-let port= process.env.port || 3000;
+let port= process.env.PORT || 3000;
 
 app.listen(port, () =>{
     console.log('servidor corriendo en puerto' +port)
